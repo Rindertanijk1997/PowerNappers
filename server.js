924b6f43761e7ddcd756a2dd71b91e9964430ec7
katerinajoanna <kasiapotar@gmail.com>
2024-05-30 14:57:26 +0200

    Lagt till products.js i routes, i server,js lagt till förfrågningar för bassökvägen /produkter.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import productsRoutes from './routes/products.js';
 import userRoutes from './routes/users.js';
 import authRoutes from './routes/auth.js';
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+
+app.use('/products', productsRoutes);
 
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
