53c5783b6a90ba367cf9b873c1f5c8dd4268264c
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-05-31 09:50:30 +0200

    klar

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import orderRoutes from './routes/orders.js'; 
 
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+app.use('/orders', orderRoutes);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
