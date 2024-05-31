8d4e13071a41e3c0a13911f2608801527e91e4eb
Adam Rehal <Niklasn89@gmail.com>
2024-05-31 11:34:54 +0200

    Fixat så man kan hämta about information från menyn

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import aboutRouter from './routes/about.js'
 
 const app = express();
 const PORT = 8080;
+
+app.use('/about', aboutRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
