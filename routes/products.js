624c52133a49b23de221e463fcc80a1f36d3f52e
katerinajoanna <kasiapotar@gmail.com>
2024-05-31 12:13:47 +0200

    Tog bort onödiga komentar

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,11 @@
 import express from 'express';
-import fs from 'fs';
+import loadMenu from '../middlewares/loadMenu.js';
 
 const router = express.Router();
 
 // Endpoint att hämta alla produkter
-router.get('/', (req, res) => {
-    // data från menu.json
-    fs.readFile('./data/menu.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: 'Failed to load menu data' });
-        }
-        // Parsuj dane JSON
-        const menuData = JSON.parse(data);
-        // Zwróć dane jako odpowiedź
-        res.json(menuData.menu);
-    });
+router.get('/', loadMenu, (req, res) => {
+    res.json(req.menu);
 });
 
 export default router;
-
