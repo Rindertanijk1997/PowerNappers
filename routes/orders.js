c5a05b9e9d1598566ef5519599f304e375f7cd90
Jonas Stenelind <jstenelind@gmail.com>
2024-06-03 11:44:37 +0200

    småfix

--- routes/orders.js
+++ routes/orders.js
@@ -1,28 +1,26 @@
 import express from 'express';
 import Datastore from 'nedb';
-import validateProduct from '../middlewares/validateProduct.js'; // Importera middleware
+import validateProduct from '../middlewares/validateProduct.js'; 
 import errorHandler from '../middlewares/orderMiddleWare.js';
 
 
 const router = express.Router();
 const dbCart = new Datastore({ filename: './db/cart.db', autoload: true });
 
-// Funktion för att lägga till produkt i varukorgen
 export function addToCart(product, callback) {
     dbCart.insert(product, callback);
 }
 
-router.post('/add-to-cart', validateProduct, (req, res, next) => { // Använd middleware
+router.post('/add-to-cart', validateProduct, (req, res, next) => { 
     addToCart(req.body, (err, newDoc) => {
         if (err) {
-            return next(err); // Skickar fel till errorHandler
+            return next(err);
         }
         res.status(200).send({ message: 'Produkt tillagd i varukorgen', product: newDoc });
     });
 });
 
 
-// Funktion för att hämta alla produkter i varukorgen
 export function getCart(callback) {
     dbCart.find({}, callback);
 }
@@ -30,18 +28,15 @@ export function getCart(callback) {
 router.get('/cart', (req, res, next) => {
     getCart((err, products) => {
         if (err) {
-            return next(err); // Skickar fel till errorHandler
+            return next(err); 
         }
 
-        // Beräkna totalpriset för produkterna i varukorgen
         const totalPrice = products.reduce((total, product) => total + Number(product.price), 0);
 
-        // Skicka tillbaka produkterna tillsammans med totalpriset
         res.status(200).json({ products, totalPrice });
     });
 });
 
-// Function för att ta bort från varukorg     ---------------------------------------
 export function deleteFromCart(productId, callback) {
     dbCart.remove({ _id: productId }, {}, callback);
 }
@@ -51,7 +46,7 @@ router.delete('/cart/:id', (req, res, next) => {
 
     deleteFromCart(productId, (err, numRemoved) => {
         if (err) {
-            return next(err);  // Skickar fel till errorHandler
+            return next(err);  
         }
         if (numRemoved === 0) {
             return res.status(404).send('Produktet finns inte i varukorgen');
@@ -62,14 +57,18 @@ router.delete('/cart/:id', (req, res, next) => {
 
 
 router.post('/create-order', (req, res, next) => {
+    console.log("Route /create-order träffad");
     const orderItems = req.body.items;
     const totalAmount = orderItems.reduce((total, item) => total + item.price, 0);
+
+    const processingTime = new Date();
+    processingTime.setMinutes(processingTime.getMinutes() + 15);
 
     const order = {
         userId: req.body.userId || 'guest',
         items: orderItems,
         totalAmount: totalAmount,
-        status: 'Processing',
+        status: 'Levereras: ' + processingTime.toLocaleTimeString(), 
         createdAt: new Date()
     };
 
@@ -77,7 +76,8 @@ router.post('/create-order', (req, res, next) => {
         if (err) {
             return next(err);
         }
-        res.status(201).json({ message: 'Order skapad', orderId: newDoc._id });
+        console.log(order);
+    res.status(201).json({ message: 'Order skapad', orderId: newDoc._id, order: order });
     });
 });
 
@@ -95,7 +95,6 @@ router.get('/history/:userId', (req, res, next) => {
 });
 
 
-// Använd errorHandler middleware
 router.use(errorHandler);
 
 export default router;
